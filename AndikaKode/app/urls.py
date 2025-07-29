9569f97670e1d10cfdcca7742a2e18f15d4e314f
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2025-07-29 13:50:38 +0200

    .

--- AndikaKode/app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("aimable", views.aimable, name="aimable"),
-    # let's create some parametirized paths
-    path("<str:name>", views.greet, name="greet"),
-]
--- AndikaKode/newyear/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-import datetime
-
-# Create your views here.
-def index(request):
-    now = datetime.datetime.now()
-    return render(request, "newyear/index.html", {"newyear": False})
